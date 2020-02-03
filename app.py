2e53e2f78b67cba0c269696dcb098f65a39ec4f1
Romar Plummer <rplummer@atlassian.com>
2020-02-02 19:14:45 -0500

    created my first Flask app

--- app.py
+++ app.py
@@ -22,6 +22,10 @@ def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 if __name__ == '__main__':
     app.run()
